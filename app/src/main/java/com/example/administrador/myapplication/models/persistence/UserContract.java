6fe01af55218cda7163cd22ae1d266feef2f9c4e
jaquelinemendes <jaquelinemendes2@yahoo.com.br>
2015-05-28 19:11:03 -0300

    teste

---
+++ app/src/main/java/com/example/administrador/myapplication/models/persistence/UserContract.java
@@ -0,0 +1,48 @@
+package com.example.administrador.myapplication.models.persistence;
+
+/**
+ * Created by Administrador on 28/05/2015.
+ */
+public class UserContract {
+
+    public static final String TABLE = "user_client";
+    public static final String ID = "id";
+    public static final String USER = "user";
+    public static final String PASSWORD = "password";
+
+
+    public static final String[] COLUNS = {ID, TABLE, USER, PASSWORD};
+
+    public static String createTable() {
+        final StringBuilder sql = new StringBuilder();
+        sql.append(" CREATE TABLE ");
+        sql.append(TABLE);
+        sql.append(" ( ");
+        sql.append(ID + " INTEGER PRIMARY KEY, ");
+        sql.append(USER + " TEXT, ");
+        sql.append(PASSWORD + " TEXT, ");
+        sql.append(" ); ");
+        return sql.toString();
+    }
+
+    public static String insertUser() {
+        final StringBuilder sql = new StringBuilder();
+        sql.append("INSERT INTO ");
+        sql.append(TABLE);
+        sql.append(" ( ");
+        sql.append(USER);
+        sql.append(",");
+        sql.append(PASSWORD);
+        sql.append(") ");
+        sql.append("VALUES ");
+        sql.append("(");
+        sql.append("Jaqueline Mendes,");
+        sql.append("cast123");
+        sql.append(")");
+
+
+        return sql.toString();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/administrador/myapplication/models/persistence/UserRepository.java
@@ -0,0 +1,37 @@
+package com.example.administrador.myapplication.models.persistence;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.administrador.myapplication.util.AppUtil;
+
+
+
+public class UserRepository {
+
+
+    private static class Singleton {
+        public static final UserRepository INSTANCE = new UserRepository();
+    }
+
+    private UserRepository() {
+        super();
+    }
+
+    public static UserRepository getInstance() {
+        return Singleton.INSTANCE;
+    }
+
+    public boolean verifyPass(String user, String pass) {
+        boolean userValid;
+        DatabaseHelper helper = new DatabaseHelper(AppUtil.CONTEXT);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String where = UserContract.USER + " = ? AND " + UserContract.PASSWORD + " = ?";
+        String[] args = {user, pass};
+        Cursor cursor = db.query(UserContract.TABLE, UserContract.COLUNS, where, args, null, null, UserContract.USER);
+        userValid = cursor.moveToFirst();
+        db.close();
+        helper.close();
+        return userValid;
+    }
+}
